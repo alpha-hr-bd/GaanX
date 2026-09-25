ac409cc4d5e2ce6b21de67bfa01b9ac423765fc4
AlphaHR <ershadaislam@gmail.com>
2026-09-25 11:48:01 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsvf_Qgz4-YgJp3wEmp68WfI15pU1ILcM",
+  authDomain: "gaan-x.firebaseapp.com",
+  projectId: "gaan-x",
+  storageBucket: "gaan-x.firebasestorage.app",
+  messagingSenderId: "1048829074592",
+  appId: "1:1048829074592:web:cef98eed3594f7f6973dc5"
+};
